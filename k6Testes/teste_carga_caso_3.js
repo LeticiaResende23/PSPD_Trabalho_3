2a341d88cef2e7bea2c560b6b902815a3ec937a1
JuliaSSouza <juliasantanna.souza@gmail.com>
2025-12-06 15:32:25 -0300

    ajustes e readme

--- k6Testes/teste_carga_caso_3.js
+++ k6Testes/teste_carga_caso_3.js
@@ -1,7 +1,7 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-const url = 'http://127.0.0.1:38491/'
+const url = 'http://127.0.0.1:5000/'
 
 const intervalo = '20s'
 const carga = 400
